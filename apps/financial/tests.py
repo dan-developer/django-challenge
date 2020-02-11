34e8022a962dc73d9dccfa0a22ce5ac85a7c5fbb
Dan <and.dan19@gmail.com>
2020-02-11 12:45:04 -0300

    Tests

--- apps/financial/tests.py
+++ apps/financial/tests.py
@@ -1,3 +1,103 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from apps.financial.models import Transaction
+from apps.financial.serializers import TransactionSerializer
 
-# Create your tests here.
+
+class TransactionTest(TestCase):
+    """
+    Test for transaction API
+    """
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='Dan', email='and.dan19@gmail.com', password='super_secret_password'
+        )
+        self.transaction = Transaction.objects.create(
+            user=self.user,
+            type=Transaction.TYPE_IN,
+            value=25487451.45
+        )
+
+    def test_validate_single_transaction(self):
+        response = self.client.get(reverse('financial:transaction-detail', args=[self.transaction.pk]))
+        transaction = Transaction.objects.get(pk=self.transaction.pk)
+        serializer = TransactionSerializer(transaction)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(float(response.data.get('value')), 25487451.45)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_transaction(self):
+        response = self.client.post(reverse('financial:transaction-list'), {
+            'user': self.user.pk,
+            'value': 459.7,
+            'type': 2
+        })
+        self.assertEqual(float(response.data.get('value')), 459.7)
+        self.assertEqual(int(response.data.get('type')), 2)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_update_transaction(self):
+        response = self.client.post(reverse('financial:transaction-list'), {
+            'user': self.user.pk,
+            'value': 459.7,
+            'type': 2
+        })
+        self.assertEqual(float(response.data.get('value')), 459.7)
+        self.assertEqual(int(response.data.get('type')), 2)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        pk = response.data.get('id')
+
+        response = self.client.put(reverse('financial:transaction-detail', args=[pk]), {
+            'user': self.user.pk,
+            'value': 559.7,
+            'type': 2
+        }, content_type='application/json')
+
+        self.assertEqual(float(response.data.get('value')), 559.7)
+        self.assertEqual(int(response.data.get('type')), 2)
+        self.assertEqual(pk, response.data.get('id'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_partial_update_transaction(self):
+        response = self.client.post(reverse('financial:transaction-list'), {
+            'user': self.user.pk,
+            'value': 98774,
+            'type': 1
+        })
+        self.assertEqual(float(response.data.get('value')), 98774)
+        self.assertEqual(int(response.data.get('type')), 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        pk = response.data.get('id')
+
+        response = self.client.patch(reverse('financial:transaction-detail', args=[pk]), {
+            'value': 98775,
+        }, content_type='application/json')
+
+        self.assertEqual(float(response.data.get('value')), 98775)
+        self.assertEqual(int(response.data.get('type')), 1)
+        self.assertEqual(pk, response.data.get('id'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_partial_delete_transaction(self):
+        response = self.client.post(reverse('financial:transaction-list'), {
+            'user': self.user.pk,
+            'value': 98774,
+            'type': 1
+        })
+        self.assertEqual(float(response.data.get('value')), 98774)
+        self.assertEqual(int(response.data.get('type')), 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        pk = response.data.get('id')
+
+        response = self.client.delete(reverse('financial:transaction-detail', args=[pk]))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_not_found_transaction(self):
+        response = self.client.get(reverse('financial:transaction-detail', args=[99999]))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
--- apps/users/tests.py
+++ apps/users/tests.py
@@ -1,3 +1,96 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from apps.financial.models import Transaction
+from apps.financial.serializers import TransactionSerializer
 
-# Create your tests here.
+
+class UserTest(TestCase):
+    """
+    Test for user API
+    """
+
+    def test_create_user(self):
+        response = self.client.post(reverse('users:user-list'), {
+            'username': 'dan',
+            'first_name': 'Anderson',
+            'last_name': 'Scouto da Silva',
+            'password': 'top_secret',
+            'phone': '+5551997196110'
+        })
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data.get('username'), 'dan')
+        self.assertEqual(response.data.get('is_superuser'), True)
+        self.assertEqual(response.data.get('password'), None)
+
+    def test_update_user(self):
+        response = self.client.post(reverse('users:user-list'), {
+            'username': 'dan',
+            'first_name': 'Anderson',
+            'last_name': 'Scouto da Silva',
+            'password': 'top_secret',
+            'phone': '+5551997196111'
+        }, content_type='application/json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data.get('username'), 'dan')
+        self.assertEqual(response.data.get('is_superuser'), True)
+        self.assertEqual(response.data.get('password'), None)
+
+        response = self.client.put(reverse('users:user-detail', args=[response.data.get('id')]), {
+            'username': 'dan',
+            'first_name': 'Anderson',
+            'last_name': 'Scouto da Silva',
+            'password': 'top_secret',
+            'phone': '+5551997196111'
+        }, content_type='application/json')
+
+        self.assertEqual(response.data.get('username'), 'dan')
+        self.assertEqual(response.data.get('phone'), '+5551997196111')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_partial_update_user(self):
+        response = self.client.post(reverse('users:user-list'), {
+            'username': 'dan',
+            'first_name': 'Anderson',
+            'last_name': 'Scouto da Silva',
+            'password': 'top_secret',
+            'phone': '+5551997196111'
+        }, content_type='application/json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data.get('username'), 'dan')
+        self.assertEqual(response.data.get('is_superuser'), True)
+        self.assertEqual(response.data.get('password'), None)
+
+        response = self.client.patch(reverse('users:user-detail', args=[response.data.get('id')]), {
+            'username': 'dan_another_user',
+        }, content_type='application/json')
+
+        self.assertEqual(response.data.get('username'), 'dan_another_user')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete_user(self):
+        response = self.client.post(reverse('users:user-list'), {
+            'username': 'dan',
+            'first_name': 'Anderson',
+            'last_name': 'Scouto da Silva',
+            'password': 'top_secret',
+            'phone': '+5551997196111'
+        }, content_type='application/json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data.get('username'), 'dan')
+        self.assertEqual(response.data.get('is_superuser'), True)
+        self.assertEqual(response.data.get('password'), None)
+
+        response = self.client.delete(reverse('users:user-detail', args=[response.data.get('id')]))
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_not_found_user(self):
+        response = self.client.get(reverse('users:user-detail', args=[99999]))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
